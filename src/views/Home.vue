a32668e52d646f55f208786e136f483d08070110
权 <2071675988@qq.com>
2022-12-14 22:24:20 +0800

    完成了echarts表格的开发

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row>
     <!-- 左侧区域 -->
-    <el-col :span="8">
+    <el-col :span="8" style="padding-right: 10px">
       <el-card class="box-card">
         <div class="user">
           <img src="../assets/images/user.png" />
@@ -33,8 +33,9 @@
       </el-card>
     </el-col>
     <!-- 右侧区域 -->
-    <el-col :span="16">
+    <el-col :span="16" style="padding-left: 10px">
       <div class="num">
+        <!-- 订单支付区域 -->
         <el-card
           v-for="item in countData"
           :key="item.name"
@@ -51,54 +52,30 @@
           </div>
         </el-card>
       </div>
+      <el-card style="height: 280px">
+        <!-- 折线图 -->
+        <div ref="echarts1" style="height: 280px"></div>
+      </el-card>
+      <div class="graph">
+        <el-card style="height: 260px">
+          <div ref="echarts2" style="height: 260px"></div>
+        </el-card>
+        <el-card style="height: 260px">
+          <div ref="echarts3" style="height: 240px"></div>
+        </el-card>
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
 import { getData } from '@/api'
+import * as echarts from 'echarts'
 export default {
   name: 'MyHome',
   data () {
     return {
-      tableData: [
-        {
-          name: 'oppo',
-          todayBuy: 500,
-          monthBuy: 3500,
-          totalBuy: 22000
-        },
-        {
-          name: 'vivo',
-          todayBuy: 300,
-          monthBuy: 2200,
-          totalBuy: 24000
-        },
-        {
-          name: '苹果',
-          todayBuy: 800,
-          monthBuy: 4500,
-          totalBuy: 65000
-        },
-        {
-          name: '小米',
-          todayBuy: 1200,
-          monthBuy: 6500,
-          totalBuy: 45000
-        },
-        {
-          name: '三星',
-          todayBuy: 300,
-          monthBuy: 2000,
-          totalBuy: 34000
-        },
-        {
-          name: '魅族',
-          todayBuy: 350,
-          monthBuy: 3000,
-          totalBuy: 22000
-        }
-      ],
+      tableData: [],
       tableLabel: {
         name: '手机品牌',
         todayBuy: '今日购买',
@@ -146,8 +123,121 @@ export default {
     }
   },
   mounted () {
-    getData().then((data) => {
-      console.log(data)
+    getData().then(({ data }) => {
+      // 表格数据
+      const { tableData } = data.data
+      // console.log(data.data)
+      // console.log(tableData)
+      this.tableData = tableData
+
+      // 基于准备好的DOM ,初始化echarts 实例  折线图
+      const echarts1 = echarts.init(this.$refs.echarts1)
+
+      // 指定图表的配置项和数据
+      let echarts1Option = {}
+
+      // 处理数据 xAxis
+      const { orderData, userData, videoData } = data.data
+      const xAxis = Object.keys(orderData.data[0])
+      const xAxisData = {
+        // data: xAxis,
+        data: orderData.date,
+
+      }
+
+      echarts1Option.xAxis = xAxisData
+      echarts1Option.yAxis = {}
+      echarts1Option.legend = xAxisData
+      echarts1Option.series = []
+      xAxis.forEach(key => {
+        echarts1Option.series.push({
+          name: key,
+          // map返回新数组
+          data: orderData.data.map(item => item[key]),
+          type: 'line'
+        })
+      })
+      console.log(echarts1Option)
+      // 使用刚指定的配置项和数据显示图表
+      echarts1.setOption(echarts1Option)
+      // console.log(xAxis)
+
+      // 柱状图
+      const echarts2 = echarts.init(this.$refs.echarts2)
+      const echarts2Option = {
+        legend: {
+          // 图例文字颜色
+          textStyle: {
+            color: "#333",
+          },
+        },
+        grid: {
+          left: "20%",
+        },
+        // 提示框
+        tooltip: {
+          trigger: "axis",
+        },
+        xAxis: {
+          type: "category", // 类目轴
+          data: userData.map(item => item.date),
+          axisLine: {
+            lineStyle: {
+              color: "#17b3a3",
+            },
+          },
+          axisLabel: {
+            interval: 0,
+            color: "#333",
+          },
+        },
+        yAxis: [
+          {
+            type: "value",
+            axisLine: {
+              lineStyle: {
+                color: "#17b3a3",
+              },
+            },
+          },
+        ],
+        color: ["#2ec7c9", "#b6a2de"],
+        series: [
+          {
+            name: '新增用户',
+            data: userData.map(item => item.new),
+            type: 'bar'
+          },
+          {
+            name: '活跃用户',
+            data: userData.map(item => item.active),
+            type: 'bar'
+          },
+        ],
+      }
+      echarts2.setOption(echarts2Option)
+
+      // 饼状图
+      const echarts3 = echarts.init(this.$refs.echarts3)
+      const echarts3Option = {
+        tooltip: {
+          trigger: "item",
+        },
+        color: [
+          "#0f78f4",
+          "#dd536b",
+          "#9462e5",
+          "#a6a6a6",
+          "#e1bb22",
+          "#39c362",
+          "#3ed1cf",
+        ],
+        series: [
+          { data: videoData, type: 'pie' },
+
+        ],
+      }
+      echarts3.setOption(echarts3Option)
     })
   }
 }
@@ -218,6 +308,14 @@ export default {
   .el-card {
     width: 32%;
     margin-bottom: 20px;
+  }
+}
+.graph {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  .el-card {
+    width: 48%;
   }
 }
 </style>
