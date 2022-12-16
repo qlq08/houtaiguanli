6a072633df2e673bfba4712652b77e273f11fca8
权 <2071675988@qq.com>
2022-12-16 19:01:31 +0800

    完成了用户管理页面的开发

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,18 @@ import http from '@/utils/request'
 export const getData = () => {
   return http.get('/home/getData')
 }
+
+// 获取列表数据
+export const getUser = (params) => {
+  console.log(params, 'params')
+  return http.get('/user/getUser', params)
+}
+export const addUser = (data) => {
+  return http.post('/user/add', data)
+}
+export const editUser = (data) => {
+  return http.post('/user/edit', data)
+}
+export const delUser = (data) => {
+  return http.post('/user/del', data)
+}
--- src/api/mock.js
+++ src/api/mock.js
@@ -1,6 +1,7 @@
 import Mock from 'mockjs'
 
 import homeapi from '@/api/mockServeData/home'
+import user from '@/api/mockServeData/user'
 // 定义mock请求拦截
 /* Mock.mock('/api/home/getData', function () {
   // 拦截到请求后的处理逻辑
@@ -8,4 +9,14 @@ import homeapi from '@/api/mockServeData/home'
   return 1
 }) */
 
+
 Mock.mock('/api/home/getData', homeapi.getStatisticalData)
+
+
+// 用户列表的数据
+Mock.mock('/api/user/add', 'post', user.createUser)
+Mock.mock('/api/user/edit', 'post', user.updateUser)
+Mock.mock('/api/user/del', 'post', user.deleteUser)
+
+// Mock.mock('/api/user/getUser', 'get', user.getUserList)
+Mock.mock(/api\/user\/getUser/, 'get', user.getUserList)
